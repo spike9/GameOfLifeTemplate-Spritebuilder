c240a6dac06f092d5ca3ce894522c5558613158b
spike9 <d.wyss@gmx.ch>
2014-11-24 11:30:01 +0100

    Automatically Committed for MakeGamesWithUs

---
+++ GameOfLife.spritebuilder/Creatures.h
@@ -0,0 +1,23 @@
+//
+//  Creatures.h
+//  GameOfLife
+//
+//  Created by Daniel Wyss on 24/11/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCSprite.h"
+
+@interface Creatures : CCSprite
+
+// stores the current stage of creature
+@property (nonatomic, assign) BOOL isAlive;
+
+// stores the amount of living neighbors
+@property (nonatomic, assign) NSInteger livingNeigbors;
+
+
+-(id)initCreature;
+
+
+@end
---
+++ GameOfLife.spritebuilder/Grid.m
@@ -0,0 +1,27 @@
+//
+//  Grid.m
+//  GameOfLife
+//
+//  Created by Daniel Wyss on 24/11/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "Grid.h"
+#import "Creatures.h"
+
+// these are variables that cannot be changed
+static const int GRID_ROWS = 8;
+static const int GRID_COLUMNS = 9;
+
+@implementation Grid {
+    NSMutableArray *_gridArray;
+    float _cellWidht;
+    float _cellHeight;
+    
+}
+
+
+
+
+
+@end
