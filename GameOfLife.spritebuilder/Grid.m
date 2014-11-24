760a0fb58ebc8c3cbd5092e2e09477f405db70f2
spike9 <d.wyss@gmx.ch>
2014-11-24 11:57:23 +0100

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Grid.m
+++ GameOfLife.spritebuilder/Grid.m
@@ -11,7 +11,7 @@
 
 // these are variables that cannot be changed
 static const int GRID_ROWS = 8;
-static const int GRID_COLUMNS = 9;
+static const int GRID_COLUMNS = 10;
 
 @implementation Grid {
     NSMutableArray *_gridArray;
@@ -50,7 +50,7 @@ static const int GRID_COLUMNS = 9;
         
         for (int j = 0; j < GRID_COLUMNS; j++) {
             Creature *creature = [[Creature alloc] initCreature];
-            creature.anchorPoint = ccp(-0.1,-0.1);
+            creature.anchorPoint = ccp(0,0);
             creature.position = ccp(x, y);
             [self addChild:creature];
             
